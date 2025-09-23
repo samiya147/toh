954bcf5427aa4e88e20926596e3710f6d7fd8261
samiya147 <samiyatazeen2006@gmail.com>
2025-09-23 22:32:34 +0530

    Add files via upload

---
+++ toerofhanoi.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+import java.lang.*;
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import javax.swing.border.*;
+
+
+class myframe1 extends JFrame
+{
+    JLabel nofdisc;
+    JTextField number;
+    JButton ok;
+    JPanel p;
+    myframe1()
+    {
+          nofdisc=new JLabel(" no of disc ");
+          number=new JTextField(20);
+          ok=new JButton("enter");
+          ok.setBackground(Color.black);
+          ok.setForeground(Color.white);
+          p=new JPanel();
+          p.add(nofdisc);
+          p.add(number);
+          p.add(ok);
+        setLayout(new FlowLayout());
+        getRootPane().setDefaultButton(ok);
+        Border br=BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.RED, 2)," DISC");
+        p.setBorder(br);
+        add(p);
+    }
+}
+public class toerofhanoi 
+{
+     public static void main(String[] args) 
+     { 
+           myframe1 fl=new myframe1();
+           fl.setTitle("nof of disc");
+           ImageIcon image=new ImageIcon("C:\\Users\\Wahab\\Downloads\\logo.webp");
+           fl.setIconImage(image.getImage());
+        fl.setSize(400,400);
+         fl.setVisible(true);
+         fl.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+     }    
+}
